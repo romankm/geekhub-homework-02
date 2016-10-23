dd7ee25b1eb341f949b247daa183a5e641fe4dbc
Roman Kyrii <romankyrii@gmail.com>
2016-10-23 14:29:10 +0300

    Added libraries; created script

--- script.php
+++ script.php
@@ -1,2 +1,13 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('Test log');
+$log->pushHandler(new StreamHandler('test.log', Logger::WARNING));
+$log->addRecord(Logger::INFO, 'Hello world');
+
+printf("Now: %s", Carbon::now());
